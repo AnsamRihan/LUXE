7ce9a0af191a36fa46ab10bab93c470606cc9353
AnsamRihan <ansam.rihan2014@gmail.com>
2026-07-08 22:10:25 +0300

    Create navbar section + js

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+"use strict";
+
+async function fetchCategories() {
+    try {
+        const response = await axios.get("https://dummyjson.com/products/category-list");
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching categories:", error);
+        return [];
+    }
+}
+
+// Split array into groups of a specific size
+function chunkArray(array, size) {
+    const chunks = [];
+
+    for (let i = 0; i < array.length; i += size) {
+        chunks.push(array.slice(i, i + size));
+    }
+
+    return chunks;
+}
+
+async function loadCategories() {
+    const categories = await fetchCategories();
+
+    const shopDropdownHover = document.getElementById("shopDropdownHover");
+    
+    const categoryColumns = chunkArray(categories, 6);
+    shopDropdownHover.innerHTML = `
+    <div class="grid px-4 py-5 text-sm grid-cols-${categoryColumns.length} gap-3 categories-navbar-link mx-15.5">
+    </div>
+    `;
+
+    const categoriesNavlink = document.querySelector(".categories-navbar-link");
+    if (!categoriesNavlink) return;
+    const categoriesSidebar = document.querySelector(".categories-sidebar");
+    if (!categoriesSidebar) return;
+
+    categoriesNavlink.innerHTML = categoryColumns.map(column => {
+        return `
+            <ul class="stack gap-3 items-start">
+                ${column.map(category => {
+                    return `
+                        <li>
+                            <a href="/category=${category}" 
+                               class="nav-link gap-1.5">
+                                ${category}
+                            </a>
+                        </li>
+                    `;
+                }).join("")}
+            </ul>
+        `;
+    }).join("");
+
+    categoriesSidebar.innerHTML = categories.map(category => {
+        return `
+            <li>
+                <a href="/category=${category}"
+                    class="nav-link w-full flex items-center px-4 py-2 border-b border-[#d2d2d2]">
+                    ${category}
+                </a>
+            </li>
+        `;
+    }).join("");
+}
+
+loadCategories();
